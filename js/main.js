b55fdc9ae8e6582060275ad60fc016dcad1c060f
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-18 00:17:37 -0700

    Add function to update the item done statement.

--- js/main.js
+++ js/main.js
@@ -23,13 +23,24 @@
         platesList.innerHTML = plates.map((plate, i) => {
             return `
                 <li>
-                    <input type="checkbox" data-index=${i} id="item${i}" ${plate.done} ? 'checked' : '' />
+                    <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
                     <label for="item${i}">${plate.text}</label>
                 </li>
             `;
         }).join('');
     };
 
+    const toggleDone = (e) => {
+        console.log(e.target);
+        if(!e.target.matches('input')) return;
+        const elem = e.target;
+        const index = elem.dataset.index;
+        items[index].done = !items[index].done;
+        localStorage.setItem('items', JSON.stringify(items));
+        populateList(items, itemList);
+    }
+
     addItems.addEventListener('submit', addItem);
+    itemList.addEventListener('click', toggleDone);
     populateList(items, itemList);
 }());
